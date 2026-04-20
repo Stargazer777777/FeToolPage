7cd30a98eb353682664ff014e0cde74e46a00be5
Stargazer <1601778118@qq.com>
2026-04-20 18:39:25 +0800

    update

---
+++ assets/index-legacy-Cl9k3Buu.js
@@ -0,0 +1,7 @@
+System.register(["./index-legacy--j9909Xf.js"],function(t,e){"use strict";var n,c,r;return{setters:[t=>{n=t.e,c=t.r,r=t.m}],execute:function(){
+/*!
+             * pinia v2.3.1
+             * (c) 2025 Eduardo San Martin Morote
+             * @license MIT
+             */
+const e=Symbol();var s,i;(i=s||(s={})).direct="direct",i.patchObject="patch object",i.patchFunction="patch function",t("default",function(){const t=n(!0),s=t.run(()=>c({}));let i=[],a=[];const u=r({install(t){u._a=t,t.provide(e,u),t.config.globalProperties.$pinia=u,a.forEach(t=>i.push(t)),a=[]},use(t){return this._a?i.push(t):a.push(t),this},_p:i,_a:null,_e:t,_s:new Map,state:s});return u}())}}});
